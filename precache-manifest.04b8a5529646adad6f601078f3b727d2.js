92eaed6d8a22a2f02305131d04f8e19489f2dec5
nischal <nischal.raj.budhathoki@javra.com>
2019-02-20 15:20:30 +0545

    Updates

---
+++ precache-manifest.04b8a5529646adad6f601078f3b727d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1ad33c0b7ae7cd35f0fc",
+    "url": "/static/js/main.1ad33c0b.chunk.js"
+  },
+  {
+    "revision": "e8a2c1c2d9f0b5f9f23b",
+    "url": "/static/js/2.e8a2c1c2.chunk.js"
+  },
+  {
+    "revision": "1ad33c0b7ae7cd35f0fc",
+    "url": "/static/css/main.9e0fef29.chunk.css"
+  },
+  {
+    "revision": "e8a2c1c2d9f0b5f9f23b",
+    "url": "/static/css/2.37ad9b26.chunk.css"
+  },
+  {
+    "revision": "e259ff19c46205a93aab032a62e5853c",
+    "url": "/index.html"
+  }
+];
